042dc2ca2ec8afb413dfeedb5f1042fde8fdd387
Jason Morganson <jason@morganson.me>
2010-08-28 19:02:00 -0500

    Style the login form

---
+++ styles/login.php
@@ -0,0 +1,29 @@
+
+#login h1 a {
+
+	background: none;
+}
+
+
+#loginform {
+	border: none;
+	background: none;
+	-webkit-box-shadow: none;
+}
+
+#loginform input {
+
+	border: 2px inset;
+}
+
+#nav {
+
+	text-shadow: none;
+}
+
+#backtoblog {
+
+	margin: 0;
+	border: none;
+	background: none;
+}
