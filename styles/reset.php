c87eb00782cdc3f687bb7a916b3f239a23265d11
Jason Morganson <jason@morganson.me>
2010-09-15 16:24:00 -0500

    Serve stylesheets through PHP to modularize them and allow processing

---
+++ styles/reset.php
@@ -0,0 +1,30 @@
+
+
+*, html, body {
+	margin: 0;
+	padding: 0;
+}
+
+
+* html {
+	width: auto;	
+}
+
+html,
+body {
+	width:  100%;
+	height: 100%;
+}
+
+* {
+/*	position: relative; */
+}
+
+img {
+	border: none;
+}
+
+ul li {
+	list-style: none;
+}
+
---
+++ styles/style.php
@@ -0,0 +1,23 @@
+<?php
+
+
+include_stylesheet( get_template_directory() . "/styles/reset.php"      );
+include_stylesheet( get_template_directory() . "/styles/ie/ie.php"      );
+include_stylesheet( get_template_directory() . "/styles/wpml.php"       );
+
+include_stylesheet( get_template_directory() . "/styles/common.php"     );
+include_stylesheet( get_template_directory() . "/styles/margins.php"    );
+include_stylesheet( get_template_directory() . "/styles/sizes.php"      );
+include_stylesheet( get_template_directory() . "/styles/img.php"        );
+include_stylesheet( get_template_directory() . "/styles/fonts.php", FALSE );
+include_stylesheet( get_template_directory() . "/styles/links.php"      );
+include_stylesheet( get_template_directory() . "/styles/colors.php"     );
+include_stylesheet( get_template_directory() . "/styles/effects.php"    );
+include_stylesheet( get_template_directory() . "/styles/visibility.php" );
+
+include_stylesheet( get_template_directory() . "/styles/login.php"      );
+
+include_stylesheet( get_stylesheet_directory() . "/style.css" );
+
+
+?>
